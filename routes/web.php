803e077ab0afc31c1bb716e3ad70f78af7bdce22
ParfaitShema <ivancyusa1@gmail.com>
2025-04-15 00:40:32 +0200

    Added attendance route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\SignupController;
 
+// Welcome Page
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Authentication Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Signup Routes
+Route::get('/signup', [SignupController::class, 'showSignupForm'])->name('signup');
+Route::post('/signup', [SignupController::class, 'signup']);
+
+// Protected Routes
+Route::middleware(['auth'])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Student Routes
+    Route::resource('students', StudentController::class);
+
+    // Attendance Routes
+    Route::resource('attendance', AttendanceController::class);
 });
